4fb5242ae3705cba3370825a41336aec4845117d
Olga Kitova <olga.vizgalova@bk.ru>
2021-07-02 13:39:07 +0500

    feat: add reducer func

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  menu: []
+}
+
+export const reducer = (state = initialState, action) => {
+
+  switch(action.type) {
+    case 'MENU_LOADED':
+      return {
+        menu: action.payload
+      };
+      default: 
+      return state;
+  }
+}
