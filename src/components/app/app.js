a0c186f79553aebaa336a2232253fde879bf5f5b
Olga Kitova <olga.vizgalova@bk.ru>
2021-07-04 14:47:27 +0500

    feat: add animation to loading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div style={{backgroundColor: 'rgba(204,70,70,1)', minHeight: '100vh' }} className="app">
+        <div style={{backgroundColor: '#29a745', minHeight: '100vh', overflowX: 'hidden' }} className="app">
             <AppHeader/>
             <Switch>
                 <Route path="/" component={MainPage} exact/>
--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './spinner.scss';
 
 const Spinner = () => {
-    return <div className="spinner">loading...</div>
+    return <div className="spinner">
+     <div className="container">
+        <div className="clock">
+            <div className="clock-inner">
+            <div className="clock-minutes"></div>
+            <ul className="clock-numbers">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+            </div>
+         </div>
+     </div>
+     <h2>Loading ...</h2>
+    </div>
 }
 
 export default Spinner;
